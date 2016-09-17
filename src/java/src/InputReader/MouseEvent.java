8a5d431fa971c530c763687a14d473fe0a3cf801
Jonah Chazan <person.1123.loztp@gmail.com>
2016-09-17 14:59:55 -0400

    MouseEvents serialize themselves

--- src/java/src/InputReader/MouseEvent.java
+++ src/java/src/InputReader/MouseEvent.java
@@ -1,10 +1,14 @@
 package InputReader;
 
+import com.google.gson.Gson;
+
 public class MouseEvent {
 	String type;
+	Gson gson = new Gson();
 	
 	void send() {
 		System.out.println(this.toString());
+		System.out.println(gson.toJson(this));
 	}
 	
 	public String toString() {
