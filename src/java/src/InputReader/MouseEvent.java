72edd9ec24eeb744255a2f4b01f647cd20f4675a
Jonah Chazan <person.1123.loztp@gmail.com>
2016-09-17 16:02:29 -0400

    Removed unnessary newline

--- src/java/src/InputReader/MouseEvent.java
+++ src/java/src/InputReader/MouseEvent.java
@@ -13,7 +13,7 @@ public class MouseEvent {
 	void send() {
 		System.out.println(this.toString());
 		System.out.println(gson.toJson(this));
-		network.send(gson.toJson(this) + "\n");
+		network.send(gson.toJson(this));
 	}
 	
 	public String toString() {
