72c0803cf5f864fd3ae6e8678df1e0c053220c42
Jonah Chazan <person.1123.loztp@gmail.com>
2016-09-17 23:09:05 -0400

    Added getting bounds for active window

--- src/java/src/Native/Windows/WinDraggedWindowDetector.java
+++ src/java/src/Native/Windows/WinDraggedWindowDetector.java
@@ -1,6 +1,8 @@
 package Native.Windows;
 
 import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinDef.RECT;
 import com.sun.jna.platform.win32.WinUser.GUITHREADINFO;
 
 public class WinDraggedWindowDetector {
@@ -10,8 +12,23 @@ public class WinDraggedWindowDetector {
 		info.cbSize=info.size();
 		User32.INSTANCE.GetGUIThreadInfo(0, info);
 		int flags = info.flags;
+		HWND wnd = info.hwndMoveSize;
+		RECT rect = new RECT();
+		User32.INSTANCE.GetWindowRect(wnd, rect);
+		System.out.println(rect);
 		
 		// GUI_INMOVESIZE 0x00000002    https://msdn.microsoft.com/en-us/library/ms632604(v=vs.85).aspx
 		return ((flags & 0x00000002) == 0) ? false : true;
+	}
+	
+	public static RECT activeWindowBounds() {
+		GUITHREADINFO info=new GUITHREADINFO();
+		info.cbSize=info.size();
+		User32.INSTANCE.GetGUIThreadInfo(0, info);
+		HWND wnd = info.hwndMoveSize;
+		RECT rect = new RECT();
+		User32.INSTANCE.GetWindowRect(wnd, rect);
+		
+		return rect;
 	}
 }
